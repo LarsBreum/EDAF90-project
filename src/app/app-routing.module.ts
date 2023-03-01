3c6a25a2a0f6ebc877e92840bfbf8e076cdba457
Linnéa <li4747ga-s@student.lu.se>
2023-03-01 19:05:13 +0100

    Added routing (first version), added "no page found", installed ng-bootstrap and angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ViewBookComponent } from './view-book/view-book.component';
 
-const routes: Routes = [];
-
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'cart', component: CartComponent },
+  { path: 'view-book', component: ViewBookComponent },
+  { path: 'order-confirmation', component: OrderConfirmationComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(appRoutes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ViewBookComponent } from './view-book/view-book.component';
 import { CartComponent } from './cart/cart.component';
 import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatBadgeModule} from '@angular/material/badge';
+//import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewBookComponent,
     CartComponent,
-    OrderConfirmationComponent
+    OrderConfirmationComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MatBadgeModule
+    //NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
