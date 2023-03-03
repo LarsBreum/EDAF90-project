3cad2aeef9656e88a02faf57df22e26f95af7db6
LarsBreum <lars.breum.lh@gmail.com>
2023-03-03 12:45:01 +0100

    push

--- src/app/view-book/view-book.component.ts
+++ src/app/view-book/view-book.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-view-book',
   templateUrl: './view-book.component.html',
-  styleUrls: ['./view-book.component.css']
+  styleUrls: ['./view-book.component.css'],
 })
-export class ViewBookComponent {
+export class ViewBookComponent implements OnInit {
+  constructor(
+    private bookService: BookService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    console.log(this.activatedRoute.snapshot.paramMap.get('isbn'));
+  }
 }
